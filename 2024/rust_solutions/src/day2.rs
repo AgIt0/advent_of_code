ebed9ca57eba8bf40cf15d18b248197ded3d6883
Lyuben Petrov <lyuben.y.petrov@gmail.com>
2024-12-10 10:57:29 +0200

    Part 2

--- 2024/rust_solutions/src/day2.rs
+++ 2024/rust_solutions/src/day2.rs
@@ -1,40 +1,61 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn part1() -> i32 {
+fn lines_reader() -> io::Lines<io::BufReader<File>> {
     let file = File::open("input/day2.txt").unwrap();
-    let reader = io::BufReader::new(file);
-    let lines = reader.lines();
+    io::BufReader::new(file).lines()
+}
+
+fn check_requirements(result: i32, direction: i32) -> bool {
+    if (1..=3).contains(&result.abs()) {
+        if (direction == 1 && result > 0) || (direction == -1 && result < 0) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    true
+}
+
+fn check_safe_report(values: &Vec<&str>) -> bool {
+    let mut direction = 0;
+
+    for values in values.windows(2) {
+        let a = values[0].parse::<i32>().unwrap();
+        let b = values[1].parse::<i32>().unwrap();
+        let result = a - b;
+        if direction == 0 {
+            if result < 0 {
+                direction = 1;
+            } else {
+                direction = -1;
+            }
+        }
+        if !check_requirements(result, direction) {
+            return false;
+        }
+    }
+    true
+}
+
+fn count_safe_reports(allow_single_bad_level: Option<bool>) -> i32 {
+    let allow_single_bad_level = allow_single_bad_level.unwrap_or(false);
 
     let mut safe_reports = 0;
-    for line in lines {
-        let mut direction = 0;
-        let mut all_safe = true;
+    for line in lines_reader() {
+        let line = line.unwrap();
+        let values = line.split_whitespace().collect::<Vec<&str>>();
+        let all_safe = check_safe_report(&values);
 
-        for values in line
-            .unwrap()
-            .split_whitespace()
-            .collect::<Vec<&str>>()
-            .windows(2)
-        {
-            let a = values[0].parse::<i32>().unwrap();
-            let b = values[1].parse::<i32>().unwrap();
-            let result = a - b;
-
-            if (1..=3).contains(&result.abs()) {
-                if direction == 0 {
-                    if result < 0 {
-                        direction = 1;
-                    } else {
-                        direction = -1;
-                    }
-                } else if (direction == 1 && result > 0) || (direction == -1 && result < 0) {
-                    all_safe = false;
+        if !all_safe && allow_single_bad_level {
+            for i in 0..values.len() {
+                let mut values = values.clone();
+                values.remove(i);
+                if check_safe_report(&values) {
+                    safe_reports += 1;
                     break;
                 }
-            } else {
-                all_safe = false;
-                break;
             }
         }
 
@@ -46,7 +67,15 @@ fn part1() -> i32 {
     safe_reports
 }
 
+fn part1() -> i32 {
+    count_safe_reports(None)
+}
+
+fn part2() -> i32 {
+    count_safe_reports(Some(true))
+}
+
 pub fn solve() {
     println!("Part 1 - {}", part1());
-    // println!("Part 2 - {}", part2());
+    println!("Part 2 - {}", part2());
 }
